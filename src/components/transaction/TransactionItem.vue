b7386769ef3cc5d1f7c6c1e07f7ca032f8228587
flibustier <contact@jonathan.pl>
2018-06-19 17:17:37 +0200

    Little inversion correction

--- src/components/transaction/TransactionItem.vue
+++ src/components/transaction/TransactionItem.vue
@@ -4,18 +4,18 @@
     <div class="px-2">{{ date }}</div>
   <div class="d-flex flex-wrap">
     <div class="px-2">
-      <span v-bind:class="{'badge badge-light': transaction.from == address, 'badge badge-info': transaction.from != address }">
-        <span class="d-lg-none">{{ toSlice }}</span>
-        <span class="d-none d-lg-block">{{transaction.to}}</span>
+      <span v-bind:class="{'badge badge-light': transaction.from !== address, 'badge badge-info': transaction.from === address }">
+        <span class="d-lg-none">{{ fromSlice }}</span>
+        <span class="d-none d-lg-block">{{transaction.from}}</span>
       </span>
     </div>
     <div class="px-2">
       <i class="fas fa-arrow-right"></i>
     </div>
     <div class="px-2">
-      <span v-bind:class="{'badge badge-info': transaction.from == address, 'badge badge-light': transaction.from != address }">
-        <span class="d-lg-none">{{ fromSlice }}</span>
-        <span class="d-none d-lg-block">{{transaction.from}}</span>
+      <span v-bind:class="{'badge badge-info': transaction.to === address, 'badge badge-light': transaction.to !== address }">
+        <span class="d-lg-none">{{ toSlice }}</span>
+        <span class="d-none d-lg-block">{{transaction.to}}</span>
       </span>
     </div>
   </div>
